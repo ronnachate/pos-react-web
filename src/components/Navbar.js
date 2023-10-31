d7f2d761ef1e1cb2a5fc7fce18bda535c6a5a304
Ronnachate <ronnachate@gmail.com>
2023-10-31 23:15:49 +0700

    Add navbar menu.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,47 @@
-import { Link } from 'react-router-dom';
-import { Nav } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
-import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import { Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
+
+import './navbar.css';
 
 const NavBar = () => {
- return (
-<Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Sire name</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/user">User</Nav.Link>
+  const navigate = useNavigate();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    navigate('/');
+  };
+  return (
+    <Navbar variant="dark" bg="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="/">
+        <Image src="/images/logo.png" fluid  className='navbar-logo'/>
+        </Navbar.Brand>
+        <Navbar.Collapse className="w-100 d-flex justify-content-end">
+          <Nav>
+            <Image src="/images/profile.png" fluid  className='nav-avatar'/>
+            <NavDropdown
+              id="nav-menu-dropdown"
+              title=""
+              menuVariant="dark"
+              className="nav-avatar-dropdown"
+            >
+              <NavDropdown.Item href="#">User' Name</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/">
+                Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/">
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
- );
+  );
 };
 
 export default NavBar;
