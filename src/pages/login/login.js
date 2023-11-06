6d299e86da9cb8e5bc51678ec06a13df179c8ec6
Ronnachate <ronnachate@gmail.com>
2023-11-06 11:27:17 +0700

    Simplay add mock login flow.

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,50 +1,70 @@
+import { Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 import { Button, Form, Image } from 'react-bootstrap';
 
+import useAuth from '../../hooks/useAuth';
+
 import './login.css';
 
 const Login = () => {
   const navigate = useNavigate();
-  return (
-    <>
-      <Helmet>
-        <title>Logout</title>
-      </Helmet>
-      <div className="container-fluid login-container">
-        <div className="container mx-auto my-auto px-0 ">
-          <div className="form-container">
+  const { login, isAuth } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async () => {
+    setIsLoading(true);
+    const token = await login('username', 'password');
+    setIsLoading(false);
+    navigate('/');
+  };
+  if (!isAuth()) {
+    return (
+      <>
+        <Helmet>
+          <title>Logout</title>
+        </Helmet>
+        <div className="container-fluid login-container">
+          <div className="container mx-auto my-auto px-0 ">
+            <div className="form-container">
               <div class="logo_box mx-auto text-center">
-                    <Image src="/images/logo.png" fluid />
-                </div>
-                <h2 class="text-white text-center">Login to your Account</h2>
-            <Form>
-              <div className='form-input'>
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlInput1"
-                >
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control type="text" placeholder="username" />
-                </Form.Group>
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlInput1"
-                >
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="password" />
-                </Form.Group>
+                <Image src="/images/logo.png" fluid />
               </div>
-              <Button variant="success" type="submit">
-                Login
-              </Button>
-            </Form>
+              <h2 class="text-white text-center">Login to your Account</h2>
+              <Form>
+                <div className="form-input">
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="username" />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="password" />
+                  </Form.Group>
+                </div>
+                <Button
+                  variant="success"
+                  type="button"
+                  disabled={isLoading}
+                  onClick={handleLogin}
+                >
+                  Login
+                </Button>
+              </Form>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+  return <Navigate to={'/'} replace />;
 };
 
 export default Login;
